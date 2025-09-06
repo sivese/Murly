54bac4bf0de434b30c000842372e5ac2b1225802
Doss Lee <driversivese@gmail.com>
2025-09-06 01:11:34 -0700

    initialization script and webrtc module

---
+++ include/streaming/streamer.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+// WebRTC includes
+#include "api/create_peerconnection_factory.h"
+
+class WebRTCStreamer {
+public:
+
+private:
+
+};
---
+++ main.cc
@@ -0,0 +1,5 @@
+#include "streaming/streamer.hh"
+
+auto main(int argc, char* argv[]) -> int {
+    return 0;
+}
